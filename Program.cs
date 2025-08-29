4e69f0620091698d04121badd625681eaae0f636
ViniciusKrau <viniciuskrau@gmail.com>
2025-08-28 23:29:34 -0300

    Adicionando Scrutor servicos no builder

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-using Microsoft.Data.SqlClient;
-using TesteTecnicoApi.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IProdutoRepository, SqlProdutoRepository>();
+
+builder.Services.Scan(scan => scan
+    .FromAssemblyOf<Program>()
+        .AddClasses(classes => classes.InNamespaces("TesteTecnicoApi.Repositories"))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime()
+        .AddClasses(classes => classes.InNamespaces("TesteTecnicoApi.Service"))
+            .AsSelf()
+            .WithScopedLifetime());
 
 var app = builder.Build();
 
